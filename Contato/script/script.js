71bc6fb7909cada995ae35565c89e5773ba0f6d6
Francine Santos <francinesantoss85@gmail.com>
2023-09-07 16:54:05 -0300

    adicionado uma pasta script aonde que sera usado na tela login com um pequeno filtro

---
+++ Contato/script/script.js
@@ -0,0 +1,28 @@
+   // javascript para a formatação da senha 
+     // obs professor: peguei essa parte na internet só para fazer os 
+        //filtros conforme descrito nos comentarios 
+     
+        function togglePasswordVisibility() {
+            var senhaInput = document.getElementById('senha');
+            var senhaToggle = document.querySelector('.password-toggle');
+
+            if (senhaInput.type === 'password') {
+                senhaInput.type = 'text';
+                senhaToggle.innerHTML = '&#128064;'; // Mostrar o ícone do olho fechado
+            } else {
+                senhaInput.type = 'password';
+                senhaToggle.innerHTML = '&#128065;'; // Mostrar o ícone do olho aberto
+            }
+        }
+
+        function validatePassword() {
+            var senhaInput = document.getElementById('senha');
+            var senhaValue = senhaInput.value;
+
+            // Remover caracteres não numéricos
+            senhaValue = senhaValue.replace(/[^0-9]/g, '');
+
+            // Atualizar o valor do campo com os caracteres filtrados
+            senhaInput.value = senhaValue;
+        }
+    
